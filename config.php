c76280a431116697f1b9836331b21004a5cd5e86
upwork0228 <amya84392@gmail.com>
2024-05-01 20:19:50 -0400

    linux-apache-mysql-php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+// Database connection parameters
+$db_hostname = "localhost";
+$db_port     = 3306;
+$db_database = "simple_lamp";
+$db_username = "username";
+$db_password = "password";
+
+// Image upload options
+$storage_option = "hd";
+$hd_folder  = "uploads";
+
+// Simulate latency, in seconds
+$latency = 0;
+
+// Cache configuration
+$enable_cache = false;
+$cache_server = "dns-or-ip-of-memcached-server";
+?>
